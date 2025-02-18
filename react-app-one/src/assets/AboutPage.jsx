527c09ff2ea2ca84ab8a570e546eed7c757b969e
advaith1408 <153885737+advaith1408@users.noreply.github.com>
2025-02-18 20:18:31 +0530

    Update AboutPage.jsx

--- react-app-one/src/assets/AboutPage.jsx
+++ react-app-one/src/assets/AboutPage.jsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
     <div className="about-page">
       <h1>About Us</h1>
       <p2><img src="https://img.freepik.com/free-vector/gradient-mba-illustration_23-2149350374.jpg?semt=ais_hybrid"></img></p2>
-      <p>Welcome to VNR Notion!</p>
+      <p>Welcome to Nest Notion!</p>
       <p>
         Our platform is designed to make accessing study materials easier and more personalized for students. By simply using your unique roll number, you can quickly find and download relevant resources, including PDF notes, previous question papers (PYQs), and other essential study materials. 
       </p>
